a14b521dcddfd10149777ba2d1dc1ef463cc0efa
Evan Thomas <evanthomas0292@gmail.com>
2023-06-19 15:20:19 +0100

    delete player's ability to make move directly since this now the game's job. Delete unecessary test. Add test for accessing player symbol

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,7 @@
 class Player
   attr_accessor :symbol
-  
+
   def initialize(symbol)
     @symbol = symbol
-  end
-
-  def move(board, position)
-    board.place(@symbol, position)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,9 @@
 require 'player'
 
 RSpec.describe Player do
-  it "can place its symbol on the board" do
+  it "has a symbol" do
     player = Player.new("X")
-    fake_board = double :fake_board
 
-    expect(fake_board).to receive(:place).with("X", "B2").once
-
-    player.move(fake_board, "B2")
+    expect(player.symbol).to eq "X"
   end
 end
